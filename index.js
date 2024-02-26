966069e89664fbfc17dfe026f5576b436823361b
Emily Kent <kentembowemily@gmail.com>
2024-02-26 13:05:41 +0000

    Prompt messages added for information about the manager and engineer

--- index.js
+++ index.js
@@ -13,3 +13,61 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the employee Id for the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the email address for the team manager?'
+    },
+    {
+        type: 'input',
+        name: 'managerOfficeNumber',
+        message: 'What is the office number for the team manager?'
+    }
+]
+
+function addEngineer() {
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the engineer?'
+    },
+    {
+        type: 'input',
+        name: 'engineerId',
+        message: 'What is the employee Id for the engineer?'
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'What is the email address for the engineer?'
+    },
+    {
+        type: 'input',
+        name: 'engineerGithub',
+        message: 'What is the Github username for the engineer?'
+    }
+])
+//return to menu
+}
+
+function addIntern() {
+
+}
+
+function init () {
+    inquirer.prompt(teamMembers)
+}
+
+init()
